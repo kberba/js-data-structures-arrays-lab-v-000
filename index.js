e9348751460c3345b4cfdb260ade511d36bb452c
Karen Berba <kberba@gmail.com>
2018-02-11 02:27:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function prependDriver(name) {
 }
 
 function removeLastDriver() {
-  const byeLastDriver = drivers.slice(-1);
+  const byeLastDriver = drivers.slice(0, drivers.length - 1);
   return byeLastDriver;
 }
 
